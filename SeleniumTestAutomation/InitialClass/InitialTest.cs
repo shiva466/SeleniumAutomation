e07cee4f6daa6ff6d509ad6f421f1a95aef62ec7
shiva ayyala <avnrsaishiva@gmail.com>
2023-07-16 10:51:09 +0530

    Base working code done

---
+++ SeleniumTestAutomation/InitialClass/InitialTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+namespace SeleniumTestAutomation.InitialClass
+{
+    public class InitialTest
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Initialise()
+        {
+            // Create a new instance of the Chrome driver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [OneTimeTearDown]
+        public void Close()
+        {
+            // Close the browser
+           // driver.Quit();
+        }
+    }
+}
